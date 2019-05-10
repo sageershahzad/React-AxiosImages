89f98b8a182f4df6b80fc2e1e706ef7a1abc9ab9
Sageer Shahzad <rudph1@gmail.com>
2019-05-10 02:39:40 +0100

    Added images listing

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
         const response = await axios
             .get('https://api.unsplash.com/search/photos', {
                 params: { query: term },
                 headers: {
-                    Authorization: 'Client-ID your api key'
+                    Authorization: 'Client-ID add api'
                 }
-            })
-        console.log(response.data.results);
+            });
+        //console.log(this);
+        this.setState({ images: response.data.results });
     }
-
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         )
     }
